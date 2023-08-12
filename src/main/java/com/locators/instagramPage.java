ac3d17f67f76b1ed6bfdbf708974b64acb0be057
kirans108 <kiranswamy37@gmail.com>
2023-08-12 13:15:22 +0530

    Automation project

--- src/main/java/com/locators/instagramPage.java
+++ src/main/java/com/locators/instagramPage.java
@@ -2,13 +2,18 @@ package com.locators;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class instagramPage {
     public static void main(String[] args) throws InterruptedException {
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.navigate().to("https://www.instagram.com/");
-        Thread.sleep(3000);
+        Thread.sleep(1000);
+        //invalid credentials login
         driver.findElement(By.xpath("//*[@id=\"loginForm\"]/div[1]/div[1]/div/label/input")).sendKeys("kiran");
         driver.findElement(By.xpath("//input[@aria-label='Password']")).sendKeys("kiran@123");
         driver.findElement(By.xpath("//*[@id=\"loginForm\"]/div/div[3]")).click();
@@ -17,6 +22,7 @@ public class instagramPage {
         Thread.sleep(3000);
         driver.findElement(By.xpath("//a[normalize-space()='Back to login']")).click();
         Thread.sleep(5000);
+        //signup
         driver.findElement(By.linkText("Sign up")).click();
         Thread.sleep(3000);
         driver.findElement(By.xpath("//button[@type='button']")).click();
@@ -29,7 +35,29 @@ public class instagramPage {
         Thread.sleep(3000);
         driver.findElement(By.xpath("//button[normalize-space()='Sign up']")).click();
         Thread.sleep(3000);
-        driver.findElement(By.xpath("//img[@alt='Get it on Google Play'][1]")).click();
-        driver.close();
+        // Providing Valid Credentials
+        // Valid Username
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("testingtestdata");
+        Thread.sleep(1000);
+        // Valid Password
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("testingtestdatablz");
+        Thread.sleep(1000);
+        // Login
+        driver.findElement(By.xpath("//div[text()='Log in']")).click();
+        Thread.sleep(2000);
+
+        //choosing option Not Now For Notifications
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()=\"Not Now\"]")));
+        driver.findElement(By.xpath("//button[text()=\"Not Now\"]")).click();
+
+        //More
+        driver.findElement(By.xpath("//span[text()='More']")).click();
+        Thread.sleep(1000);
+
+        //Log Out
+        driver.findElement(By.xpath("//html/body[1]")).click();
+        Thread.sleep(1000);
+
     }
 }
--- src/main/java/com/locators/linkedIn.java
+++ src/main/java/com/locators/linkedIn.java
@@ -1,20 +1,28 @@
 package com.locators;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
-public class SeleniumDay1 {
+public class linkedIn {
     public static void main(String[] args) throws InterruptedException {
 
             ChromeDriver driver = new ChromeDriver();
             driver.manage().window().maximize();
             driver.get("https://www.linkedin.com/");
-            Thread.sleep(3000);
+            Thread.sleep(1000);
             driver.navigate().refresh();
-            Thread.sleep(3000);
+            Thread.sleep(1000);
             driver.findElement(By.id("session_key")).sendKeys("Kiran");
-            Thread.sleep(3000);
+            Thread.sleep(1000);
             driver.findElement(By.id("session_password")).sendKeys("Kiran");
+            //perform page down page up actions
+            Actions actions = new Actions(driver);
+            actions.sendKeys(Keys.PAGE_DOWN).perform();
+            Thread.sleep(500);
+            actions.sendKeys(Keys.PAGE_UP).perform();
+            Thread.sleep(500);
             driver.findElement(By.xpath("//button[@data-id='sign-in-form__submit-btn']")).click();
             driver.findElement(By.linkText("Forgot password?")).click();
             driver.findElement(By.className("back_button")).click();
